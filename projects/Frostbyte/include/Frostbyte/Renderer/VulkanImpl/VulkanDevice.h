c321a7626e78ad39e8a67304cddb49b2654e3992
London Ball <56260362+londonmax12@users.noreply.github.com>
2023-11-09 20:56:04 +1100

    Graphics queue

--- projects/Frostbyte/include/Frostbyte/Renderer/VulkanImpl/VulkanDevice.h
+++ projects/Frostbyte/include/Frostbyte/Renderer/VulkanImpl/VulkanDevice.h
@@ -5,11 +5,18 @@
 namespace Frostbyte {
 	class VulkanDevice {
 	public:
+		struct QueueFamilyIndices {
+			uint32_t GraphicsQueue;
+		};
+
 		bool Init(VulkanContext* context);
 		void Shutdown();
 
 	private:
 		bool PickDevice(VulkanContext* context);
+		bool FindQueueFamilies(VkPhysicalDevice device);
+
+		QueueFamilyIndices m_QueueIndices;
 
 		VkPhysicalDevice m_Physical = VK_NULL_HANDLE;
 	};
--- projects/Frostbyte/src/Renderer/VulkanImpl/VulkanDevice.cpp
+++ projects/Frostbyte/src/Renderer/VulkanImpl/VulkanDevice.cpp
@@ -47,9 +47,13 @@ bool Frostbyte::VulkanDevice::PickDevice(VulkanContext* context)
 		if (!deviceFeatures.geometryShader)
 			continue;
 
-		FROSTBYTE_INFO("Found suitible device: {}", deviceProperties.deviceName);
-		FROSTBYTE_INFO("\t| Score: {}", deviceScore);
+		FROSTBYTE_INFO("Finding queue family indices");
+		if (!FindQueueFamilies(device))
+			continue;
 
+		FROSTBYTE_INFO("Found suitible device");
+		FROSTBYTE_INFO("\t| Name: {}", deviceProperties.deviceName);
+		FROSTBYTE_INFO("\t| Score: {}", deviceScore);
 
 		if (deviceScore > bestScore)
 			m_Physical = device;
@@ -61,6 +65,34 @@ bool Frostbyte::VulkanDevice::PickDevice(VulkanContext* context)
 		FROSTBYTE_ERROR("Failed to find suitable graphics device for Vulkan");
 		return false;
 	}
+
+	return true;
+}
+
+bool Frostbyte::VulkanDevice::FindQueueFamilies(VkPhysicalDevice device)
+{
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+	VkQueueFamilyProperties* queueFamilies = (VkQueueFamilyProperties*)malloc(queueFamilyCount * sizeof(VkQueueFamilyProperties));
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies);
+
+	bool foundGraphicsIndex = false;
+	QueueFamilyIndices queueIndices{};
+
+	for (int i = 0; i < queueFamilyCount; i++) {
+		if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+			queueIndices.GraphicsQueue = i;
+			foundGraphicsIndex = true;
+		}
+	}
+
+	free(queueFamilies);
+
+	if (!foundGraphicsIndex)
+		return false;
+
+	m_QueueIndices = queueIndices;
 
 	return true;
 }
